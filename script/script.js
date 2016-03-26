c2c1211df5413009899365f83c9b892e8205b22b
EmielZuurbier <emielzuurbier@outlook.com>
2016-03-27 00:22:41 +0100

    Multiple enhancements
    - Added Service Worker
    - Added AnalyserNode
    - Added Wave Visualizer
    - Added Oscillators

--- script/script.js
+++ script/script.js
@@ -5,6 +5,20 @@
     var buttons = document.querySelectorAll('.button-audio'),
         audios = document.querySelectorAll('audio');
     
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js', { scope: './' })
+            .then(function (reg) {
+                console.log('registered sw (see console)');
+                console.info('registered sw', reg);
+            })
+            .catch(function (err) {
+                console.log('error registering sw (see console)');
+                console.error('error registering sw', err);
+            });
+    } else {
+        console.error('ServiceWorker is not supported');
+    }
+    
     // CHECK IF AUDIOCONTEXT IS AVAILABLE
     if ('AudioContext' in window || 'webkitAudioContext' in window) {
         
@@ -16,6 +30,10 @@
             lowFilter = context.createBiquadFilter(),
             notchFilter = context.createBiquadFilter(),
             panFilter = context.createStereoPanner(),
+            analyser = context.createAnalyser(),
+            drawVisual,
+            canvas = document.querySelector('canvas'),
+            canvasCtx = canvas.getContext('2d'),
             bufferList = [],
             paths = [],
             playing = [],
@@ -24,7 +42,13 @@
             reset = document.getElementById('reset'),
             tempoLight = document.getElementById('tempo'),
             tempoInd = document.getElementById('tempo-indicator'),
-            handler;
+            osc = document.getElementById('osc'),
+            oscGainNode = context.createGain(),
+            oscMarkerX = document.getElementById('osc-marker-x'),
+            oscMarkerY = document.getElementById('osc-marker-y'),
+            oscArray = [],
+            handlerClick,
+            handlerMove;
         
         // SET FILTER TYPES
         highFilter.type = 'highpass';
@@ -42,8 +66,14 @@
         highFilter.connect(lowFilter);
         lowFilter.connect(notchFilter);
         notchFilter.connect(panFilter);
-        panFilter.connect(context.destination);
+        panFilter.connect(analyser);
+        analyser.connect(context.destination);
         
+        // CONFIGUR ANALYSER
+        analyser.minDecibels = -90;
+        analyser.maxDecibels = -10;
+        console.log(analyser.frequencyBinCount);
+                
         // GET LINKS FROM HREF ATTRIBUTES AND PUSH TO PATHS ARRAY
         for (var i = 0; i < buttons.length; i += 1) {
             paths.push(buttons[i].getAttribute('href'));
@@ -77,6 +107,23 @@
             request.send();
         }
         
+        // OSCILLATOR CONSTRUCTOR
+        function Oscillator(type, frequency, detune, connect) {
+            this.source = context.createOscillator();
+            this.source.type = type;
+            this.source.frequency.value = frequency;
+            this.source.detune.value = detune;
+            this.source.connect(connect);
+        }
+        
+        Oscillator.prototype.start = function () {
+            this.source.start();
+        }
+        
+        Oscillator.prototype.stop = function () {
+            this.source.stop();
+        }
+        
         // AUDIO CONSTRUCTOR
         function AudioPlayer(bufferLocation, id, name, pathToConnect, loop, onendedFunction) {
             this.source = context.createBufferSource();
@@ -105,6 +152,64 @@
             tempoLight.style.animation='blinker ' + (controls[5].getAttribute('max') - value) + 's infinite normal running';
             this.source.playbackRate.value = rate;
         };
+        
+        // VISUALIZE WAVES
+        // CODE FROM voice-change-o-matic BY mdn ON 
+        // https://github.com/mdn/voice-change-o-matic/
+        function visualize() {
+            var WIDTH = canvas.width,
+                HEIGHT = canvas.height;
+            
+            analyser.fftSize = 2048;
+            var bufferLength = analyser.fftSize;
+            var dataArray = new Uint8Array(bufferLength);
+
+            canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+            function draw() {
+                
+                var grd = canvasCtx.createLinearGradient(0,0,0,170);
+                grd.addColorStop(0,"#194A19");
+                grd.addColorStop(1,"#282B27");
+
+                drawVisual = requestAnimationFrame(draw);
+
+                analyser.getByteTimeDomainData(dataArray);
+
+                canvasCtx.fillStyle = grd;
+                canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+                canvasCtx.lineWidth = 2;
+                canvasCtx.strokeStyle = '#427b42';
+
+                canvasCtx.beginPath();
+
+                var sliceWidth = WIDTH * 1.0 / bufferLength;
+                var x = 0;
+
+                for(var i = 0; i < bufferLength; i++) {
+
+                    var v = dataArray[i] / 128.0;
+                    var y = v * HEIGHT/2;
+
+                    if(i === 0) {
+                        canvasCtx.moveTo(x, y);
+                    } else {
+                    canvasCtx.lineTo(x, y);
+                    }
+
+                    x += sliceWidth;
+                }
+
+                canvasCtx.lineTo(canvas.width, canvas.height/2);
+                canvasCtx.stroke();
+            }
+
+            draw();
+        }
+        
+        // START THE VIUSALIZER
+        visualize();
         
         // STOP PLAYING
         function stopSound(event) {
@@ -179,12 +284,10 @@
         
         // RESET CONTROLS
         function resetControls(event) {
-            controls[0].value = 500;
-            controls[1].value = 0;
-            controls[2].value = 10000;
-            controls[3].value = 0;
-            controls[4].value = 0;
-            controls[5].value = 0.46875;
+            var values = [500, 0, 10000, 0, 0, 0.46875];
+            for (var i = 0; i < controls.length; i += 1) {
+                controls[i].value = values[i];
+            }
             
             gainNode.gain.value = controls[0].value / 500;
             highFilter.frequency.value = controls[1].value;
@@ -194,6 +297,81 @@
             event.preventDefault();
         }
         
+        // START OSCILLATOR
+        function oscPlayer(event) {
+            if (oscArray.length === 0) {
+                                
+                // SHOW DIAGRAM
+                oscMarkerX.classList.toggle('hidden');
+                oscMarkerY.classList.toggle('hidden');
+                
+                // GET SELECTED OSCILLATOR TYPE
+                var oscType = document.getElementById('oscillator-type'),
+                    oscTypeSelected = oscType.options[oscType.selectedIndex].value;
+            
+                // CREATE NEW OSCILLATORS
+                var toneC = new Oscillator(
+                    oscTypeSelected, 
+                    261.63, 
+                    100, 
+                    analyser
+                );
+
+                var toneE = new Oscillator(
+                    oscTypeSelected,
+                    329.63,
+                    100,
+                    analyser
+                );
+
+                var toneG = new Oscillator(
+                    oscTypeSelected,
+                    392.00,
+                    100,
+                    analyser
+                );
+                
+                oscArray.push(toneC, toneE, toneG);
+                
+                // PLAY THE OSCILLATOR
+                for (var i = 0; i < oscArray.length; i += 1) {
+                    oscArray[i].start();
+                }
+                
+            } else {
+                
+                // HIDE DIAGRAM
+                oscMarkerX.classList.toggle('hidden');
+                oscMarkerY.classList.toggle('hidden');
+                
+                // STOP ALL OSCILLATORS
+                for (var i = 0; i < oscArray.length; i += 1) {
+                    oscArray[i].stop();
+                }
+                
+                oscArray = [];
+                
+            }
+        }
+        
+        // OSCILLATOR CONTROL
+        function oscControl(event) {
+            var oscFreqValue = [261.63, 329.63, 392.00];
+            
+            oscMarkerX.style.transform = 'translate(' + event.layerX + 'px, ' + 0 + 'px)';
+            oscMarkerY.style.transform = 'translate(' + 0 + 'px, ' + event.layerY + 'px)';
+            
+            for (var i = 0; i < oscArray.length; i += 1) {
+                
+                // X AXIS FREQUENCY CHANGE
+                oscArray[i].source.frequency.value = oscFreqValue[i] + event.layerX;
+                
+                // Y AXIS DETUNE CHANGE
+                oscArray[i].source.detune.value = -100 + event.layerY;
+            }
+        }
+                
+        // TOGGLE KEYPRESS ANIMATION
         function toggle(event) {
             buttons[event.target.id].classList.toggle('active');
         }
@@ -304,18 +482,20 @@
         
         // SEE IF TOUCHEVENTS ARE SUPPORTED
         if ('ontouchstart' in document.documentElement) {
-            handler = 'touchstart';
+            handlerClick = 'touchstart';
+            handlerMove = 'touchmove';
         } else {
-            handler = 'click';
+            handlerClick = 'click';
+            handlerMove = 'mousemove'
         }
         
         // ADD EVENTLISTENERS TO BUTTONS EXCEPT THE LAST BUTTON
         for (var i = 0; i < buttons.length -1; i += 1) {
-            buttons[i].addEventListener(handler, createSound, false);
+            buttons[i].addEventListener(handlerClick, createSound, false);
         }
         
         // ADD EVENTLISTENER FOR MELODY BUTTON
-        buttons[buttons.length -1].addEventListener(handler, createMelody, false);
+        buttons[buttons.length -1].addEventListener(handlerClick, createMelody, false);
         
         // ADD EVENTLISTENERS TO CONTROLS
         for (var i = 0; i < controls.length; i += 1) {
@@ -325,8 +505,10 @@
         // ADD EVENTLISTENERS TO ADDITIONAL CONTROLS
         stop.addEventListener('click', stopSound, false);
         reset.addEventListener('click', resetControls, false);
+        osc.addEventListener('click', oscPlayer, false);
         document.addEventListener('keydown', keypress, false);
         document.addEventListener('keyup', keypressUp, false);
+        osc.addEventListener(handlerMove, oscControl, false);
         
     } else if ('addEventListener' in window) {
 
@@ -338,9 +520,9 @@
             var audioPlayer = audios[event.target.id];
             audioPlayer.currentTime = 0;
             audioPlayer.play();
-            console.log(audioPlayer);
             
             event.preventDefault();
+            event.returnValue = false;
         }
         
         // PLAY WITH AUDIOTAG
@@ -351,7 +533,6 @@
             } else {
                 audioPlayer.play();
             }
-            console.log(audioPlayer.paused);
             event.preventDefault();
             event.returnValue = false;
         }
